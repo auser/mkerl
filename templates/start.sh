81d0622519f9deec0eab6af63ea937fb1d391567
Ari Lerner <arilerner@mac.com>
2010-04-29 11:29:58 -0700

    Updated start.sh to include deps directories

--- templates/start.sh
+++ templates/start.sh
@@ -2,9 +2,12 @@
 
 VERSION=<%= version %>
 PWD=$(dirname $0)
+DEPS_DIRS=$(find $PWD/deps -maxdepth 1 -mindepth 1 -type d | tr '\n' ' ')
+PA_DIRS=$(for dir in $DEPS_DIRS; do echo "-pa $dir"; done)
 CONFIG=$1
 
 erl -pa $PWD/ebin \
+    $PA_DIRS \
     -s reloader \
     -<%= name %> \
     -boot <%=name %>-$VERSION
